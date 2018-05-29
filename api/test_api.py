dba94f87a5b054194d901af4458b8813ccd1c1a9
RutaleIvanPaul <paulivan568@gmail.com>
2018-05-29 19:23:06 -0400

    Create API directory with Virtual environment

---
+++ api/test_api.py
@@ -0,0 +1,18 @@
+import unittest
+import apifunctions
+
+class TestApi(unittest.TestCase):
+
+    def test_createRequest(self):
+        #Test whether the API function enables user to create a request
+    
+    def test_getRequest(self):
+        #Test whether the API fucntion enables user to get a given request
+
+    def test_getRequests(self):
+        #Test whether the API function enables user to get requests for all logged in users
+
+    def test_modifyRequest(self):
+        #Test whether the API function enables user to modify given request
+
+
