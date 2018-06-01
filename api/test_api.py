336acff9c4cadb1a30783f6469ba50166a7d03a9
RutaleIvanPaul <paulivan568@gmail.com>
2018-06-01 15:07:33 -0400

    [Finishes #157971179]Add edge tests

--- api/test_api.py
+++ api/test_api.py
@@ -28,68 +28,69 @@ class TestApi(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create_request(self):
-        #ensure no missing values
-        user_request = {'id': 5,'userID':5,'description':'pc over heats even on low activity'}
-        response = self.app.post(BASE_URL,
-                                 data=json.dumps(user_request),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-
-        #ensure title is string
-        user_request = {'id': 5,'userID':5,'title':1,'description':'pc over heats even on low activity'}
-        response = self.app.post(BASE_URL,
-                                 data=json.dumps(user_request),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-
-        #ensure description is string
-        user_request = {'id': 5,'userID':5,'description':2}
-        response = self.app.post(BASE_URL,
-                                 data=json.dumps(user_request),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-
-        #ensure no repeated requests
-        user_request = {'id': 6,'userID':5,'title': 'pc over heat','description':'pc over heats even on low activity'}
-        response = self.app.post(BASE_URL,
-                                 data=json.dumps(user_request),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-
-        #ensure correct request passes
         user_request = {'id': 6,'userID':5,'title': 'new request','description':'pc over heats even on low activity'}
         response = self.app.post(BASE_URL,
                                  data=json.dumps(user_request),
                                  content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
+    def test_missing_values(self):
+        user_request = {'id': 5,'userID':5,'description':'pc over heats even on low activity'}
+        response = self.app.post(BASE_URL,
+                                 data=json.dumps(user_request),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_is_title_string(self):
+        user_request = {'id': 5,'userID':5,'title':1,'description':'pc over heats even on low activity'}
+        response = self.app.post(BASE_URL,
+                                 data=json.dumps(user_request),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_is_desscription_string(self):
+        user_request = {'id': 5,'userID':5,'description':2}
+        response = self.app.post(BASE_URL,
+                                 data=json.dumps(user_request),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_no_repeated_strings(self):
+        user_request = {'id': 6,'userID':5,'title': 'pc over heat','description':'pc over heats even on low activity'}
+        response = self.app.post(BASE_URL,
+                                 data=json.dumps(user_request),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        
+
     def test_modify_request(self):
-        #ensure no editing non existing requests
+        modify_request = {"description":"New Description"}
+        response = self.app.put(BASE_URL+"/2",
+                                data=json.dumps(modify_request),
+                                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    def test_editing_nonexisting_requests(self):
         modify_request = {"title":"New title","description":"New Description"}
         response = self.app.put(BASE_URL+"/7",
                                 data=json.dumps(modify_request),
                                 content_type='application/json')
         self.assertEqual(response.status_code, 400)
         
-        #ensure description or title is not a non string
+    def test_modify_request_description_not_string(self):
         modify_request = {"description":1}
         response = self.app.put(BASE_URL+"/2",
                                 data=json.dumps(modify_request),
                                 content_type='application/json')
         self.assertEqual(response.status_code, 400)
-
+    
+    def test_modify_request_title_not_string(self):
         modify_request = {"title":1}
         response = self.app.put(BASE_URL+"/2",
                                 data=json.dumps(modify_request),
                                 content_type='application/json')
         self.assertEqual(response.status_code, 400)
         
-        #ensure correct request passes
-        modify_request = {"description":"New Description"}
-        response = self.app.put(BASE_URL+"/2",
-                                data=json.dumps(modify_request),
-                                content_type='application/json')
-        self.assertEqual(response.status_code, 200)
+        
 
     def tearDown(self):
         # reset app.requests to initial state
